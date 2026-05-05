768b938832278891d43f47284966b032a0c1d004
gauravnetes <gouravchandra935@gmail.com>
2026-05-05 12:50:27 +0530

    Rotate Image  Rotate List

---
+++ LC-POTD/may_26/05_rotate_list.cpp
@@ -0,0 +1,40 @@
+#include <vector>
+using namespace std; 
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(!head || !head->next || k == 0)  
+            return head; 
+
+        int len = 1;
+        ListNode* tail = head; 
+
+        while(tail->next) {
+            len++; 
+            tail = tail->next; 
+        }
+
+        k = k % len; 
+        if(k == 0) return head; 
+        
+        ListNode* newTail = head; 
+        for(int i = 0; i < len - k - 1; i++) {
+            newTail = newTail->next; 
+        }
+
+        tail->next = head; 
+        head = newTail->next; 
+        newTail->next = NULL; 
+
+        return head; 
+    }
+};
