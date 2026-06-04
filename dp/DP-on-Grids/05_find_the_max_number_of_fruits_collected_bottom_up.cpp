0a20f9ee2f2ee7474269823234ba0d2454527cce
gauravnetes <gouravchandra935@gmail.com>
2026-06-04 11:05:45 +0530

    Find the Maximum Number of Fruits Collected --bottom up

---
+++ dp/DP-on-Grids/05_find_the_max_number_of_fruits_collected_bottom_up.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+
+using namespace std; 
+
+class Solution {
+public:
+
+    int maxCollectedFruits(vector<vector<int>>& fruits) {
+        int n = fruits.size();
+        vector<vector<int>> t(n, vector<int>(n, 0));  
+        int res = 0; 
+        for(int i = 0; i < n; i++) {
+            res += fruits[i][i]; 
+        }
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i < j && i+j < n-1) {
+                    t[i][j] = 0; 
+                } else if(i > j && i+j < n-1) {
+                    t[i][j] = 0; 
+                } else {
+                    t[i][j] = fruits[i][j];
+                }
+            }
+        } 
+
+        for(int i = 1; i < n; i++) {
+            for(int j = i+1; j < n; j++) {
+                t[i][j] += max({t[i-1][j-1], t[i-1][j], (j+1 < n) ? t[i-1][j+1] : 0});
+            }
+        }
+
+        for(int j = 1; j < n; j++) {
+            for(int i = j+1; i < n; i++) {
+                t[i][j] += max({t[i-1][j-1], t[i][j-1], (i+1 < n) ? t[i+1][j-1] : 0}); 
+            }
+        }
+
+        return res + t[n-1][n-2] + t[n-2][n-1]; 
+    }
+};
